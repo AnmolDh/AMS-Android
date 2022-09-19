5a9c5d17d5b7453072df2f727e7b715a4393a34a
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2022-09-19 15:28:10 +0530

    created activty 2

---
+++ app/src/main/java/com/projects/amsystem/calculateAttendance.java
@@ -0,0 +1,14 @@
+package com.projects.amsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calculateAttendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_attendance);
+    }
+}
