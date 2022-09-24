8aeed2e5e6e37708598cc44c453dee6a1c495b7d
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2022-09-24 12:08:14 +0530

    calculation code and impl

--- app/src/main/java/com/projects/amsystem/MainActivity.java
+++ app/src/main/java/com/projects/amsystem/MainActivity.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.getStarted);
-        button.setOnClickListener(new View.OnClickListener(){
+        getStarted = (Button) findViewById(R.id.getStarted);
+        getStarted.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openNewActivity();
--- app/src/main/java/com/projects/amsystem/calculateAttendance.java
+++ app/src/main/java/com/projects/amsystem/calculateAttendance.java
@@ -2,13 +2,40 @@ package com.projects.amsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class calculateAttendance extends AppCompatActivity {
 
+    EditText totalClassesInput, classesAttendedInput;
+    Button calculateBtn;
+    TextView calculatedValues;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_attendance);
+
+        totalClassesInput = findViewById(R.id.totalClassesInput);
+        classesAttendedInput = findViewById(R.id.classesAttendedInput);
+        calculateBtn = findViewById(R.id.calculateBtn);
+        calculatedValues = findViewById(R.id.calculatedValues);
+
+        calculateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                float tc = Float.parseFloat(totalClassesInput.getText().toString());
+                float ca = Float.parseFloat(classesAttendedInput.getText().toString());
+
+                calculatedValues.setText("Your Attendance: " + ((ca/tc)*100) + "%");
+            }
+        });
     }
 }
